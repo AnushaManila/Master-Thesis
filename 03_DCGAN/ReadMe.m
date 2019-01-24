28280c53e911966d10ec50e9bd6ad4049e7d3631
AnushaManila <anushamanila9@gmail.com>
2019-01-24 09:52:58 -0800

    Create ReadMe.m
    Brief description of DCGAN experiments

---
+++ 03_DCGAN/ReadMe.m
@@ -0,0 +1,39 @@
+# Generation of Faces with GANs-The DCGAN
+
+The experiments on altering the intermediate layers of DCGAN network and extending the model architecture.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See acknowledgement for details on how to deploy the project on a live system.
+
+### Prerequisites
+Development Environment:
+```
+	Ubuntu 16.04 LTS
+	NVIDIA Titan X (Pascal/Maxwell)/Quadro P5000
+	cuda 8.0
+	Python 2.7.6
+	pytorch 0.1.12
+	torchvision 0.1.8
+	matplotlib 1.3.1
+	imageio 2.2.0
+	Tensorboard 1.6.0
+```
+
+### Details of the DCGAN experiments
+
+The experiments are classified into five categories:
+
+```
+1. Transposed convolution to upsample images
+2. Up-sampling based on interpolations
+3. Changing the depth of intermediate layers
+4. Altering the latent space dimension
+5. Extending the DCGAN architecture
+```
+
+## Acknowledgment
+
+* https://github.com/znxlwm/pytorch-MNIST-CelebA-GAN-DCGAN
+
+
